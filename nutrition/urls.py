11e70190b88f27a5560d70ad7d45acf51a339264
Richard Hanks <richardhanksjr@gmail.com>
2020-06-10 13:18:32 -0400

    Changed project name and related references from musicstudy to nutrition.

--- nutrition/urls.py
+++ nutrition/urls.py
@@ -1,4 +1,4 @@
-"""musicstudy URL Configuration
+"""nutrition URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
