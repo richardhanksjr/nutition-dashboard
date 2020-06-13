624e757dccabc6fa81ff81c50faddc21af2b0799
Richard Hanks <richardhanksjr@gmail.com>
2020-06-13 07:50:43 -0400

    Moved the nutrition entry form into the index template.

--- front_end/static/requests.js
+++ front_end/static/requests.js
@@ -1,6 +1,8 @@
 let pe_ratio = document.getElementById("pe_ratio");
 let pe_ratio_content = document.getElementById("pe_ratio_content");
 
+let nutrition_entry = document.getElementById("nutrition_entry");
+
 function fetchHTMLandAppend(parent_id, route){
     fetch(route)
         .then(function(response) {
@@ -9,6 +11,7 @@ function fetchHTMLandAppend(parent_id, route){
         .then(function(html){
             let parser = new DOMParser();
             let doc = parser.parseFromString(html, 'text/html');
+            console.log(doc)
             let pe_element = doc.getElementById(parent_id);
             pe_ratio_content.innerHTML = pe_element.innerHTML;
         })
@@ -16,3 +19,4 @@ function fetchHTMLandAppend(parent_id, route){
 
 
 pe_ratio.addEventListener("click", () => fetchHTMLandAppend('pe_ratio_parent', "api/pe_ratio"));
+nutrition_entry.addEventListener("click", () => fetchHTMLandAppend("nutrition_entry_form", "api/nutrition_entry_load"));
--- front_end/views.py
+++ front_end/views.py
@@ -1,7 +1,14 @@
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from api.forms import NutritionEntryForm
+
 
 class Index(LoginRequiredMixin, TemplateView):
     template_name = 'front_end/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = NutritionEntryForm
+        return context
 
